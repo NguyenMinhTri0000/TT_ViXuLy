0df58f66b517c88479069491f812cf7a5947130d
NMT <nguyenminhtri.20122001@gmail.com>
2022-10-11 17:02:39 +0700

    20221011

---
+++ BAI_511_DEM_GIAY.c
@@ -0,0 +1,29 @@
+
+#include <tv_pickit2_shift_1.c>
+
+signed INT8 giay;
+unsigned INT16 i;
+
+void hienthi_delay()
+{
+   FOR (i=0;i<200;i++)
+   {
+      xuat_8led_7doan_quet_2(0,ma7doan [giay %10]);
+      delay_us(200);
+      xuat_8led_7doan_quet_tat_led();
+      xuat_8led_7doan_quet_2(1,ma7doan [giay/10]);
+      delay_us(200);
+      xuat_8led_7doan_quet_tat_led();
+   }
+}
+
+void main()
+{
+   set_up_port_ic_chot();
+
+   WHILE(true)
+   {
+      FOR(giay =0; giay <60; giay ++) hienthi_delay();
+   }
+}
+
